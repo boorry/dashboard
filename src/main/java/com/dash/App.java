21c90428c88bae5fe9da677a55e890752dd67ec5
boorry <ellessraharivelo@gmail.com>
2025-10-29 22:03:38 +0300

    début

---
+++ src/main/java/com/dash/App.java
@@ -0,0 +1,13 @@
+package com.dash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
