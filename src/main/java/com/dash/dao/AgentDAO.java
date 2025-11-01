7134fb66dd1fdb7c1a0c91d7a8ad06e5afbebcba
boorry <ellessraharivelo@gmail.com>
2025-11-01 22:06:25 +0300

    connection agent

--- src/main/java/com/dash/dao/AgentDAO.java
+++ src/main/java/com/dash/dao/AgentDAO.java
@@ -3,17 +3,37 @@ package com.dash.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.dash.model.Agent;
 import java.sql.*;
 
-public class AgentDAO{
-    
+public class AgentDAO {
 
+    public Agent login (String login, String password) {
+        String sql = "SELECT * FROM agents WHERE login = ? AND password = ?";
+        
+        DatabaseConnection connect = new DatabaseConnection();
+         
+        try (Connection conn = connect.getConnection()) {
 
-
-
-    public AgentDAO(){
-
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+             
+            pstmt.setString(1, login);
+            pstmt.setString(2, password); // À hasher plus tard !
+            
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Agent agent = new Agent();
+                agent.setId(rs.getInt("id"));
+                agent.setLogin(rs.getString("login"));
+                agent.setNom(rs.getString("nom"));
+                agent.setPrenom(rs.getString("prenom"));
+                return agent;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null; // Échec
     }
 
+    public AgentDAO () {}
 }
-
